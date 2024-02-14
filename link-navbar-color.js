22ee18b214cb458b42789be1b47c6bc540a2a09e
AlberTorres <albertotos64@gmail.com>
2024-02-14 10:00:22 -0500

    plantilla final para todo

--- link-navbar-color.js
+++ link-navbar-color.js
@@ -1,27 +1,47 @@
-const general = document.querySelectorAll(".link-unique");
-const enlaces = document.querySelectorAll(".color");
-const primerEnlace = enlaces[0];
-const segundoEnlace = enlaces[1];
-const tercerEnlace = enlaces[2];
-const cuartoEnlace = enlaces[3];
+$(document).ready(function () {
+  // Función para manejar el desplazamiento y el clic en enlaces de la barra lateral
+  function handleNavigation() {
+    var scrollPos = $(document).scrollTop();
 
-const mostrarColorEnlace = (entradas, observador,validacion) => {
-  entradas.forEach((entrada) => {
-    if (entrada.isIntersecting) {
-      segundoEnlace.classList.remove("colorObserver");
-      primerEnlace.classList.add("colorObserver");
-    } else {
-      primerEnlace.classList.remove("colorObserver");
-      segundoEnlace.classList.add("colorObserver");
-    }
+    $("h2[id]").each(function () {
+      const sectionId = $(this).attr("id");
+      const sectionTop = $(this).offset().top;
+      const sectionBottom = sectionTop + $(this).outerHeight();
+      const windowHeight = $(window).height();
+      const windowCenter = windowHeight / 3.8;
+
+      // Verifica si el centro de la sección está en el viewport
+      if (
+        scrollPos + windowCenter >= sectionTop &&
+        scrollPos + windowCenter <= sectionBottom
+      ) {
+        // Aplica estilos cuando la sección está en el centro durante el desplazamiento
+        $("#subtitle a.page1").removeClass("page1");
+        $("#subtitle a").removeClass("active");
+        $('#subtitle a[href="#' + sectionId + '"]').addClass("active");
+      }
+    });
+  }
+
+  // Manejar el desplazamiento inicial
+  handleNavigation();
+
+  // Manejar el desplazamiento
+  $(window).scroll(function () {
+    handleNavigation();
   });
-};
 
-const observador = new IntersectionObserver(mostrarColorEnlace, {
-  root: null,
-  rootMargin: "0px 0px 0px 0px",
-  threshold: 0.27,
+  // Manejar el clic en enlaces de la barra lateral
+  $("#subtitle a.color").on("click", function (event) {
+    event.preventDefault();
+    var target = $(this).attr("href");
+    var offset = $(target).offset().top;
+
+    $("html, body").scrollTop(offset);
+
+    // Aplica estilos cuando se hace clic en un enlace de la barra lateral
+    $("#subtitle a").removeClass("active");
+    $("#subtitle a.color").removeClass("page1");
+    $(this).addClass("page1");
+  });
 });
-
-
-observador.observe(general[0]);
